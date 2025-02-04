e5d2f83f0c489b3c81e55ccaee969287b0d8c326
Manish Kumar <manishsbrlreso@gmail.com>
2025-02-04 16:44:30 +0530

    Version 4.3 - Removing Image Generator and Translator

--- lib/model/home_type.dart
+++ lib/model/home_type.dart
@@ -2,57 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../screen/feature/chatbot_feature.dart';
-import '../screen/feature/image_feature.dart';
-import '../screen/feature/translator_feature.dart';
-import '../screen/feature/aqi_feature.dart'; // Import AQI feature
+import '../screen/feature/aqi_feature.dart';
 
-enum HomeType {
-  aiChatBot,
-  aiImage,
-  aiTranslator,
-  aiAQICalculator
-} // Add AQI Calculator
+enum HomeType { aiChatBot, aiAQICalculator }
 
 extension MyHomeType on HomeType {
   // Title
   String get title => switch (this) {
         HomeType.aiChatBot => 'AI ChatBot',
-        HomeType.aiImage => 'AI Image Creator',
-        HomeType.aiTranslator => 'Language Translator',
-        HomeType.aiAQICalculator => 'AQI Calculator', // AQI title
+        HomeType.aiAQICalculator => 'AQI Calculator',
       };
 
   // Lottie animation file
   String get lottie => switch (this) {
         HomeType.aiChatBot => 'ai_hand_waving.json',
-        HomeType.aiImage => 'ai_play.json',
-        HomeType.aiTranslator => 'ai_ask_me.json',
-        HomeType.aiAQICalculator => 'ai_aqi_calculator.json', // AQI animation
+        HomeType.aiAQICalculator => 'ai_aqi_calculator.json',
       };
 
   // Alignment for layout
   bool get leftAlign => switch (this) {
         HomeType.aiChatBot => true,
-        HomeType.aiImage => false,
-        HomeType.aiTranslator => true,
-        HomeType.aiAQICalculator => false, // Set alignment for AQI
+        HomeType.aiAQICalculator => false,
       };
 
   // Padding for layout
   EdgeInsets get padding => switch (this) {
         HomeType.aiChatBot => EdgeInsets.zero,
-        HomeType.aiImage => const EdgeInsets.all(20),
-        HomeType.aiTranslator => EdgeInsets.zero,
-        HomeType.aiAQICalculator =>
-          const EdgeInsets.all(10), // Set padding for AQI
+        HomeType.aiAQICalculator => const EdgeInsets.all(10),
       };
 
   // Navigation for each feature
   VoidCallback get onTap => switch (this) {
         HomeType.aiChatBot => () => Get.to(() => const ChatBotFeature()),
-        HomeType.aiImage => () => Get.to(() => const ImageFeature()),
-        HomeType.aiTranslator => () => Get.to(() => const TranslatorFeature()),
-        HomeType.aiAQICalculator => () =>
-            Get.to(() => const AQIFeature()), // AQI navigation
+        HomeType.aiAQICalculator => () => Get.to(() => const AQIFeature()),
       };
 }
